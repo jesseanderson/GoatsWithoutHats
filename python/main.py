d5a80b6beaa7d9b36690616f17d3fc45d9a7897a
Jesse Anderson <jesseranders@gmail.com>
2014-09-12 21:38:14 -0700

    Start of vision tracking

--- python/main.py
+++ python/main.py
@@ -11,29 +11,51 @@ class Vision(object):
       self.cap = cv2.VideoCapture(str(sys.argv[1]))
     else: #Webcam input
       self.cap = cv2.VideoCapture(0)
-    self.players = []
+    self.tracking = [(1,0)]
 
   def run(self):
     while(self.cap.isOpened()):
       ret, frame = self.cap.read()
       cv2.imshow('Goats Without Hats!', frame)
+      """Start Tracking"""
+      if self.tracking:
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        for color, player in self.tracking:
+          if color == 0: #RED
+            lower_color = np.array([0,50,50])
+            upper_color = np.array([20,255,255])
+          elif color == 1: #BLUE
+            lower_color = np.array([110,50,50])
+            upper_color = np.array([130,255,255])
+          elif color == 2: #ORANGE
+            lower_color = np.array([30,50,50])
+            upper_color = np.array([50,255,255])
+          mask = cv2.inRange(hsv_frame, lower_color, upper_color)
+          res = cv2.bitwise_and(frame, frame, mask= mask)
+          
+      """End Tracking"""
       if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     self.cap.release()
     cv2.destroyAllWindows()
+
+  def start_tracking(self, color, player):
+    self.trackin
 
 class Game(object):
 
   def __init__(self, **kwargs):
     super(Game, self).__init__(**kwargs)
     self.players = []
+    self.player_count = 0
     self.vision = Vision()
 
   def run(self):
     self.vision.run()
     
   def add_player(self, color):
-    pass
+    self.vision.start_tracking(color, self.player_count)
+    self.player_count = self.player_count + 1
 
   def send_status(self, color):
     pass
