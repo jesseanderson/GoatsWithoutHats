78f08cb55ca246f53d2356968f8da5f47e2ffe92
Jesse Anderson <jesseranders@gmail.com>
2014-09-12 17:13:58 -0700

    outline for base methods

--- python/main.py
+++ python/main.py
@@ -1,3 +1,18 @@
+import sys
+
+"""Global Constants go here in CAPS"""
 
 class main(object):
-  pass
+
+  def __init__(self, **kwargs):
+    """Do we need this?  I don't know."""
+    super(main, self).__init(**kwargs)
+
+  def opencv_file(self):
+    pass
+
+  def opencv_webcam(self):
+    pass
+
+  def run(self):
+    pass
