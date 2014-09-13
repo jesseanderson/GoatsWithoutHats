1d653f2d8606a024f1753ba0911660ccd7c97b9f
Jesse Anderson <jesseranders@gmail.com>
2014-09-12 19:55:06 -0700

    Added Game class

--- python/main.py
+++ python/main.py
@@ -5,13 +5,13 @@ import cv2
 """Global Constants go here in CAPS"""
 
 class Vision(object):
-
   def __init__(self, **kwargs):
     super(Vision, self).__init__(**kwargs)
     if(len(sys.argv) > 1): #Video File input
       self.cap = cv2.VideoCapture(str(sys.argv[1]))
     else: #Webcam input
       self.cap = cv2.VideoCapture(0)
+    self.players = []
 
   def run(self):
     while(self.cap.isOpened()):
@@ -22,5 +22,21 @@ class Vision(object):
     self.cap.release()
     cv2.destroyAllWindows()
 
+class Game(object):
+
+  def __init__(self, **kwargs):
+    super(Game, self).__init__(**kwargs)
+    self.players = []
+    self.vision = Vision()
+
+  def run(self):
+    self.vision.run()
+    
+  def add_player(self, color):
+    pass
+
+  def send_status(self, color):
+    pass
+
 if __name__ == '__main__':
-  Vision().run()
+  Game().run()
